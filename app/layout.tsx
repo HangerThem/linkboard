7d170bef9771b619c242599101fec282ff1ecbe7
Jacob <techsupport@kreindler.ca>
2024-08-28 01:05:34 -0400

    feat: added profile as favicon (#5)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/react";
-import data from "@/config";
-import ThemeContext from "@/context/themeContext";
-import "@/styles/global.css";
-import "@/styles/normalize.css";
+import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
+import data from '@/config';
+import ThemeContext from '@/context/themeContext';
+import '@/styles/global.css';
+import '@/styles/normalize.css';
 
 export const metadata: Metadata = {
   title: data.title,
-  description: "A link board, like a bulletin board, but for links.",
+  icons: ['/profile.png'],
+  description: 'A link board, like a bulletin board, but for links.',
   authors: [
     {
-      name: "HangerThem",
-      url: "https://hangerthem.com",
+      name: 'HangerThem',
+      url: 'https://hangerthem.com',
     },
   ],
-  keywords: ["link", "board", "linkboard", "bulletin", "bulletin"],
-  creator: "HangerThem",
-  publisher: "HangerThem",
+  keywords: ['link', 'board', 'linkboard', 'bulletin', 'bulletin'],
+  creator: 'HangerThem',
+  publisher: 'HangerThem',
 };
 
 export default function RootLayout({
